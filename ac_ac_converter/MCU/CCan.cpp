b4596c5da7ce2b7a773e3b409e19a06e10126a0f
Dmitry Ivanov <dmitsi@rambler.ru>
2021-05-31 11:41:36 +0300

    It is added the new class CCan

---
+++ ac_ac_converter/MCU/CCan.cpp
@@ -0,0 +1,9 @@
+// Date: 2021.05.31
+// Creator: ID
+
+#include "CCan.h"
+
+
+class CCan : public IPheriphery
+{
+};
---
+++ ac_ac_converter/MCU/CCan.h
@@ -0,0 +1,14 @@
+// Date: 2021.05.31
+// Creator: ID
+
+#ifndef _CCAN_H
+#define _CCAN_H
+
+#include "IPheriphery.h"
+#include "plib035_can.h"
+
+
+
+
+
+#endif
